9d9581800a09e877bb2c8d7b89d3780d4b6606cd
justintsugranes <justintsugranes@proton.me>
2023-01-11 07:34:52 -0500

    add login button

---
+++ client/components/GoogleLogin.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GoogleLogin = () => {
+  return (
+    <div>
+      <button className="bg-white text-xl text-[#f51997] border-[1px] border-[#f51997] font-semibold px6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#f51997] cursor-pointer">
+        Login (PLACEHOLDER)
+      </button>
+    </div>
+  )
+}
+
+export default GoogleLogin
